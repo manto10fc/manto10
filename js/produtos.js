c5b5ebf38bda6f7c25d32825b5f63d15a093352d
mathh09-cloud <bremateus4@gmail.com>
2026-07-10 16:05:22 -0300

    Refiz estoque a pronta entrega

--- js/produtos.js
+++ js/produtos.js
@@ -11,10 +11,10 @@ const produtos = [
 
     estoque: {
       prontaEntrega: {
-        P: 1,
-        M: 1,
-        G: 1,
-        GG: 1
+        P: 0,
+        M: 0,
+        G: 0,
+        GG: 0
       },
       encomenda: ["P", "M", "G", "GG"]
     },
@@ -48,10 +48,10 @@ const produtos = [
 
     estoque: {
       prontaEntrega: {
-        P: 1,
-        M: 1,
-        G: 1,
-        GG: 1
+        P: 0,
+        M: 0,
+        G: 0,
+        GG: 0
       },
       encomenda: ["P", "M", "G", "GG"]
     },
@@ -85,10 +85,10 @@ const produtos = [
 
     estoque: {
       prontaEntrega: {
-        P: 1,
-        M: 1,
-        G: 1,
-        GG: 1
+        P: 0,
+        M: 0,
+        G: 0,
+        GG: 0
       },
       encomenda: ["P", "M", "G", "GG"]
     },
@@ -122,10 +122,10 @@ const produtos = [
 
     estoque: {
       prontaEntrega: {
-        P: 1,
-        M: 1,
-        G: 1,
-        GG: 1
+        P: 0,
+        M: 0,
+        G: 0,
+        GG: 0
       },
       encomenda: ["P", "M", "G", "GG"]
     },
